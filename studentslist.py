af5c1f05e9d941db0f1482f3597c913e184e47c6
fguiro2019 <fadelguiro@gmail.com>
2019-09-08 02:39:02 +0200

    First Commit

---
+++ studentslist.py
@@ -0,0 +1,17 @@
+#On import la library openPyXL
+import openpyxl
+
+#Ouvrir le fichier excel
+excelDocument = openpyxl.load_workbook('Eleves.xlsx')
+print(type(excelDocument))
+#Acceder a une feuille
+sheet = excelDocument.worksheets[0]
+cell = sheet['E2'].value
+print(type(cell))
+
+#Ici nous allons creer des classeur
+#wb = openpyxl.Workbook()
+
+#sheet = wb.active
+#sheet.title = 'data'
+#wb.save('Moyenne.xlsx')
